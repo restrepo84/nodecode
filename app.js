15004d3487d30ccfcb7ca46f4d8acbc14bbff3f6
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-05-26 15:06:42 -0600

    Sending requests and response

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-const userName = "Max";
+// const userName = "Max";
 
-fs.writeFile("user-data.txt", "Name: " + userName, (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log("WROTE FILE");
+// fs.writeFile("user-data.txt", "Name: " + userName, (err) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log("WROTE FILE");
+// });
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("INCOMING REQUEST");
+  console.log(req.method, req.url);
+
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Success!</h1>");
 });
+
+server.listen(5000);
